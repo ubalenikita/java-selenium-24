e68887e8e1c4a9e53b21d5b8cdd552c92ef32ab0
ubalenikita <nikitaubale121@gmail.com>
2022-11-04 15:36:51 +0530

    java course

---
+++ may-selenium-23/binary/DPTest.java
@@ -0,0 +1,40 @@
+package TestNgPrograms;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class DPTest {
+	public WebDriver driver;
+	  @Test(dataProvider = "dp")
+	  public void f(String userid, String password) {
+			System.setProperty("webdriver.edge.driver", "C:\\Users\\HP\\Desktop\\my-app-selenium\\binary\\msedgedriver.exe");
+			WebDriver driver = new EdgeDriver();
+		  driver.manage().window().maximize();
+		  driver.get("http://www.testyou.in/Login.aspx");
+		  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		  WebElement UserName1 = driver.findElement(By.xpath("//input[@id='ctl00_CPHContainer_txtUserLogin']"));
+		  WebElement Password1 = driver.findElement(By.xpath("//input[@id='ctl00_CPHContainer_txtPassword']"));
+		  WebElement StaySignIn = driver.findElement(By.xpath("//input[@id='ctl00_CPHContainer_chkRememberMe']"));
+		  WebElement LogInBtn = driver.findElement(By.xpath("//input[@id='ctl00_CPHContainer_btnLoginn']"));
+		  UserName1.sendKeys(userid);
+		  Password1.sendKeys(password);
+		  StaySignIn.click();
+		  LogInBtn.click();
+	  }
+
+	  @DataProvider
+	  public Object[][] dp() {
+	    return new Object[][] {
+	      new Object[] { "omkarbawkar@gmail.com", "a12567" },
+	      new Object[] { "omkarbawkar123@gmail.com", "1456b" },
+	      new Object[] { "8845795213", "14gs56b" },
+	    };
+	  }
+	}
---
+++ may-selenium-23/src/test/java/com/mycompany/app/may_selenium_23/NewTest.java
@@ -0,0 +1,50 @@
+package com.mycompany.app.may_selenium_23;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.println("welcome to maven project");
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+  @BeforeSuite
+  public void beforeSuite() {
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+  }
+
+}
